a114d7c2c8462c2495ce33221cafd06cc8225c22
sHimanshu22 <himanshusonawane022@gmail.com>
2026-02-16 22:55:13 +0530

    portfolio Starts

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import ThemeToggle from '../components/themeToggle'
+import StarBackGround from '../components/StarBackGround'
+
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+    {/* Theme Toggle */}
+    <ThemeToggle/>
+
+
+    {/* BackGround Effect */}
+    <StarBackGround/>
+
+    
+    {/* Navbar */}
+
+    {/* Main Content */}
+
+    {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
